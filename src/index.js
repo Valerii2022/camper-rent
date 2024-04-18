06d108ae81679aaf084befb2257beddff5719811
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-04-18 19:57:12 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <BrowserRouter basename="/camper-rent">
-  <BrowserRouter>
+  <BrowserRouter basename="/camper-rent">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
